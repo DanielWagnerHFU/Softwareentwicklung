028e2f7c572d679f02d4842e97baeafef3a0579b
DanielWagnerHFU <45201769+DanielWagnerHFU@users.noreply.github.com>
2022-04-04 14:13:42 +0200

    changes

--- Lesson/Program.cs
+++ Lesson/Program.cs
@@ -1,8 +1,41 @@
-﻿MyClass i = new MyClass{value = 42};
-MyClass j = i;
-i.value = 43;
-Console.WriteLine(j.value);
-public class MyClass
+﻿Vertex[] myVertices = new Vertex[100_000];
+Random rand = new Random();
+
+for(int i = 0; i < myVertices.Length; i++){
+    myVertices[i] = new Vertex{
+        Position = new Vec3D{
+            x = rand.NextSingle(),
+            y = rand.NextSingle(),
+            z = rand.NextSingle()
+        },
+        Normal = new Vec3D{
+            x = rand.NextSingle(),
+            y = rand.NextSingle(),
+            z = rand.NextSingle()
+        },
+        TexCoord = new Vec2D{
+            x = rand.NextSingle(),
+            y = rand.NextSingle()
+        }
+    };
+}
+
+public class Vertex
 {
-    public int value;
+    public Vec3D Position;
+    public Vec3D Normal;
+    public Vec2D TexCoord;
+}
+
+public class Vec3D
+{
+    public float x;
+    public float y;
+    public float z;
+}
+
+public class Vec2D
+{
+    public float x;
+    public float y;
 }
---
+++ Lesson2/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+A a = new A();
+A b = new B();
+a.M();
+b.M();
+
+
+public class A {
+    public virtual void M() {
+        Console.WriteLine("1");
+    }
+}
+
+public class B : A {
+    public override void M() {
+        Console.WriteLine("2");
+    }
+}
